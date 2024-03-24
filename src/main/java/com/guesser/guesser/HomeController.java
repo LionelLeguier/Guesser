51cffa972bc3878f65fb5d592406f8c83954175e
Lionel <lionelleguier@hotmail.fr>
2024-03-24 21:19:21 +0100

    Ajout du controller de difficulté et de la page de sélection de difficulté

---
+++ src/main/java/com/guesser/guesser/HomeController.java
@@ -0,0 +1,25 @@
+package com.guesser.guesser;
+import org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "difficulte"; // Renvoie la page de choix de la difficulté
+    }
+
+    @PostMapping("/start-game")
+    public String startGame(@RequestParam("difficulte") String difficulte, HttpSession session) {
+        // Enregistrer la difficulté choisie dans la session
+        session.setAttribute("difficulte", difficulte);
+        // Rediriger vers la page de jeu
+        return "redirect:/toto";
+    }
+}
